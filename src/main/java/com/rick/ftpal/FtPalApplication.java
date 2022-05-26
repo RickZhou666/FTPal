e92a8b734a613f6cf3cf2564664f130e1e789df7
RunZhou <runzhou@RunZhous-MacBook-Pro.local>
2022-05-26 17:22:13 +0800

    initial commit

---
+++ src/main/java/com/rick/ftpal/FtPalApplication.java
@@ -0,0 +1,13 @@
+package com.rick.ftpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtPalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtPalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rick/ftpal/FtPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick.ftpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtPalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
