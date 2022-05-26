126e8aeb8665e7801ad8568879a86e0e70a212c9
RunZhou <runzhou@RunZhous-MacBook-Pro.local>
2022-05-26 22:04:24 +0800

    clear springboot startup error

--- src/main/java/com/rick/ftpal/FtPalApplication.java
+++ src/main/java/com/rick/ftpal/FtPalApplication.java
@@ -1,9 +1,12 @@
 package com.rick.ftpal;
 
+import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProviderImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@SpringBootApplication
 public class FtPalApplication {
 
     public static void main(String[] args) {
